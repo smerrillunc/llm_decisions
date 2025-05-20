fa9b5b03302751a7ba56912e7141911cd934fe16
Scott Merrill <scottmerrill@Mac-Pro.local>
2025-05-20 19:26:11 -0400

    update default params

--- diarizationScript.py
+++ diarizationScript.py
@@ -7,11 +7,12 @@ def main():
     parser = argparse.ArgumentParser(description='Zoom Meeting Speaker Diarization')
     parser.add_argument('--video_file', help='Path to zoom video')
     parser.add_argument('--save_dir', default='/work/users/s/m/smerrill/Albemarle/transcripts', help='Path to directory to save transcripts')
-    parser.add_argument('--sampling_rate', '-s', type=float, default=1.0,
+    parser.add_argument('--sampling_rate', '-s', type=float, default=2.0,
                         help='Frames per second to sample (default: 1)')
     
     args = parser.parse_args()
-
+    os.makedirs(args.save_dir, exists_ok=True)
+    
     print(f"Attempting to Download {args.video_file}")
     try:
         save_name = os.path.join(args.save_dir, args.video_file.split('/')[-1].replace('.mp4', '.txt'))
