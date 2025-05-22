c989b0b60507b50fcf391d69ff25db716b012a41
Scott Merrill <scottmerrill@Mac-Pro.local>
2025-05-22 16:48:52 -0400

    Updating transcript creation logic

--- diarizationScript.py
+++ diarizationScript.py
@@ -6,7 +6,7 @@ def main():
     """Main function to run the diarization"""   
     parser = argparse.ArgumentParser(description='Zoom Meeting Speaker Diarization')
     parser.add_argument('--video_file', help='Path to zoom video')
-    parser.add_argument('--save_dir', default='/work/users/s/m/smerrill/Albemarle/transcripts', help='Path to directory to save transcripts')
+    parser.add_argument('--save_dir', default='/work/users/s/m/smerrill/Albemarle/speakerChanges', help='Path to directory to save transcripts')
     parser.add_argument('--sampling_rate', '-s', type=float, default=2.0,
                         help='Frames per second to sample (default: 1)')
     
@@ -18,7 +18,13 @@ def main():
         
         diarizer = ZoomSpeakerDiarization(args.video_file)
         diarized_transcript = diarizer.process_video(sampling_rate=args.sampling_rate)
-        diarizer.save_transcript(diarized_transcript, save_name)
+        # no longer using this basic logic to construct transcripts
+        # diarizer.save_transcript(diarized_transcript, save_name)
+
+        # instead just save the video mappings
+        save_name = os.path.join(args.save_dir, args.video_file.split('/')[-1].replace('.mp4', '.npy'))
+        np.save(save_name, diarizer.speaker_changes)
+
     except Exception as e:
         print(e)
 
